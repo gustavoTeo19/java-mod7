88f6c7d1589e6f516e563fcb4e61ab08eaf162be
GustavoTeo <62210677+GustavoTeo@users.noreply.github.com>
2022-05-26 16:05:27 -0300

    first commit

---
+++ src/ComputadorClasse.java
@@ -0,0 +1,58 @@
+public class ComputadorClasse {
+    private String gabinete;
+    private String monitor;
+    private String teclado;
+    private String mouse;
+    private String placaMae;
+    private String placaVideo;
+
+    /*Esse é um comentário*/
+
+    public String getGabinete() {
+        return gabinete;
+    }
+
+    public void setGabinete(String gabinete) {
+        this.gabinete = gabinete;
+    }
+
+    public String getMonitor() {
+        return monitor;
+    }
+
+    public void setMonitor(String monitor) {
+        this.monitor = monitor;
+    }
+
+    public String getTeclado() {
+        return teclado;
+    }
+
+    public void setTeclado(String teclado) {
+        this.teclado = teclado;
+    }
+
+    public String getMouse() {
+        return mouse;
+    }
+
+    public void setMouse(String mouse) {
+        this.mouse = mouse;
+    }
+
+    public String getPlacaMae() {
+        return placaMae;
+    }
+
+    public void setPlacaMae(String placaMae) {
+        this.placaMae = placaMae;
+    }
+
+    public String getPlacaVideo() {
+        return placaVideo;
+    }
+
+    public void setPlacaVideo(String placaVideo) {
+        this.placaVideo = placaVideo;
+    }
+}
---
+++ src/PrimeiraClasse.java
@@ -0,0 +1,7 @@
+public class PrimeiraClasse {
+    public static void main(String args[]){
+        ComputadorClasse computador = new ComputadorClasse();
+        computador.setGabinete("Rise Mode Glass");
+        System.out.println(computador.getGabinete());
+    }
+}
